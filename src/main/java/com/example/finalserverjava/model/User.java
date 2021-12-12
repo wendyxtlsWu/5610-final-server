f74ce68bcd73dc838eafaac17315473a4c03100e
Fulia Bai <fulia@Fulias-MacBook-Pro.local>
2021-12-11 22:51:33 -0800

    login and register

---
+++ src/main/java/com/example/finalserverjava/model/User.java
@@ -0,0 +1,68 @@
+package com.example.finalserverjava.model;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+    private String username;
+    private String password;
+    private Integer zipCode;
+    private String email;
+    private String userType;
+
+    public User() {
+    }
+
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String pwd) {
+        this.password = pwd;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getZipCOde() {
+        return zipCode;
+    }
+
+    public void setZipCode(Integer zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String type) {
+        this.userType = type;
+    }
+}
+
