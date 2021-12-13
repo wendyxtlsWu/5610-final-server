ce8573462be4f2e1cc6944333a6b3fcc33d52c0f
wujingjing <wendyxtls@gmail.com>
2021-12-12 20:55:18 -0800

    create pet model

---
+++ src/main/java/com/example/finalserverjava/model/Pet.java
@@ -0,0 +1,103 @@
+package com.example.finalserverjava.model;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "pets")
+public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String size;
+    private String age;
+    private String photo;
+    private String type;
+    private String gender;
+    private Boolean need_shelter;
+    private Integer userId;
+    private Integer userName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Boolean getNeed_shelter() {
+        return need_shelter;
+    }
+
+    public void setNeed_shelter(Boolean need_shelter) {
+        this.need_shelter = need_shelter;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getUserName() {
+        return userName;
+    }
+
+    public void setUserName(Integer userName) {
+        this.userName = userName;
+    }
+
+}
